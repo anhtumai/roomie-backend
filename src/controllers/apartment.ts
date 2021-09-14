fa37d49cb5f579b5da152214e0834f429164f632
anhtumai <tu.mai@outlook.com>
2021-09-15 00:03:13 +0300

    Update apartment controller for API v2

--- src/controllers/apartment.ts
+++ src/controllers/apartment.ts
@@ -2,36 +2,16 @@ import { Router } from 'express'
 import { Prisma } from '@prisma/client'
 
 import apartmentModel from '../models/apartment'
+import accountModel from '../models/account'
 import middleware from '../util/middleware'
 import logger from '../util/logger'
 import { RequestAfterExtractor } from '../types/express-middleware'
 import processClientError from '../util/error'
-import membershipModel from '../models/membership'
 import invitationModel from '../models/invitation'
 
-const apartmentRouter = Router()
+const apartmentsRouter = Router()
 
-apartmentRouter.get(
-    '/',
-    middleware.accountExtractor,
-    async (req: RequestAfterExtractor, res, next) => {
-        try {
-            const apartment = await membershipModel.findApartment(req.account.id)
-            if (apartment === null)
-                return processClientError(
-                    res,
-                    404,
-                    'This account is not member of any apartments',
-                )
-            return res.status(200).json(apartment)
-        } catch (err) {
-            logger.error(err)
-            next(err)
-        }
-    },
-)
-
-apartmentRouter.post(
+apartmentsRouter.post(
     '/',
     middleware.accountExtractor,
     async (req: RequestAfterExtractor, res, next) => {
@@ -43,11 +23,13 @@ apartmentRouter.post(
                 req.body.name,
                 req.account.id,
             )
-            await membershipModel.create(req.account.id, newApartment.id)
+            await accountModel.update(
+                { id: req.account.id },
+                { apartmentId: newApartment.id },
+            )
             await invitationModel.deleteMany({ inviteeId: req.account.id })
             return res.status(201).json(newApartment)
         } catch (err) {
-            logger.error(err)
             if (err instanceof Prisma.PrismaClientKnownRequestError) {
                 if (err.code === 'P2014') {
                     return processClientError(
@@ -62,7 +44,7 @@ apartmentRouter.post(
     },
 )
 
-apartmentRouter.delete(
+apartmentsRouter.delete(
     '/:id',
     middleware.accountExtractor,
     middleware.paramsIdValidator,
@@ -72,28 +54,30 @@ apartmentRouter.delete(
         const { account } = req
 
         try {
-            const deletedApartment = await apartmentModel.find({ id })
-            if (deletedApartment === null)
+            const toDeleteApartment = await apartmentModel.find({ id })
+            if (toDeleteApartment === null)
                 return processClientError(
                     res,
                     404,
                     `Apartment with id ${id} does not exist`,
                 )
-            if (deletedApartment.adminId !== account.id) {
+            if (toDeleteApartment.adminId !== account.id) {
                 return processClientError(
                     res,
                     403,
                     'User is forbidden to remove this apartment',
                 )
             }
-            await membershipModel.deleteMany({ apartmentId: id })
+            await accountModel.updateMany(
+                { apartmentId: toDeleteApartment.id },
+                { apartmentId: null },
+            )
             await apartmentModel.deleteOne({ id })
             return res.status(204).json()
         } catch (err) {
-            logger.error(err)
             next(err)
         }
     },
 )
 
-export default apartmentRouter
+export default apartmentsRouter
