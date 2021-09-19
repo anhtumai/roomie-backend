94e4a2dee20f07a86699d4341cb87170a554d589
anhtumai <tu.mai@outlook.com>
2021-09-19 21:35:20 +0300

    controllers, add update delete and view permission validator middleware for /api/tasks

--- src/controllers/task.ts
+++ src/controllers/task.ts
@@ -5,6 +5,7 @@ import _ from 'lodash'
 import taskModel from '../models/task'
 import taskRequestModel from '../models/taskRequest'
 import taskAssignmentModel from '../models/taskAssignment'
+import apartmentModel from '../models/apartment'
 
 import middleware from '../util/middleware'
 import logger from '../util/logger'
@@ -27,6 +28,17 @@ function validateTaskProperty(taskProperty: any): boolean {
     return true
 }
 
+function taskPropertyValidator(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+): Response | void {
+    if (!validateTaskProperty(request.body)) {
+        return processClientError(response, 400, 'Task property is invalid')
+    }
+    next()
+}
+
 function parseTaskProperty(
     taskProperty: any,
 ): Omit<Prisma.TaskUncheckedCreateInput, 'creator_id'> {
@@ -42,20 +54,52 @@ function parseTaskProperty(
     }
 }
 
-async function permissionValidator(
+async function updateDeletePermissionValidator(
     request: RequestAfterExtractor,
     response: Response,
     next: NextFunction,
 ): Promise<Response | void> {
     try {
         const taskId = Number(request.params.id)
+        const errorMessage =
+      'Task doesn\'t exist or user is forbidden to edit/delete this task'
         const task = await taskModel.find({ id: taskId })
-        if (task === null || task.creator_id !== request.account.id) {
-            return processClientError(
-                response,
-                403,
-                'Task doesn\'t exist or user is forbidden to edit/delete/view this task',
-            )
+        if (task !== null && task.creator_id === request.account.id) {
+            return next()
+        }
+        if (!request.account.apartment) {
+            return processClientError(response, 403, errorMessage)
+        }
+        const apartment = await apartmentModel.find({
+            id: request.account.apartment.id,
+        })
+        if (!apartment || apartment.admin_id !== request.account.id) {
+            return processClientError(response, 403, errorMessage)
+        }
+        next()
+    } catch (err) {
+        next(err)
+    }
+}
+
+async function viewPermissionValidator(
+    request: RequestAfterExtractor,
+    response: Response,
+    next: NextFunction,
+): Promise<Response | void> {
+    try {
+        const taskId = Number(request.params.id)
+        const errorMessage =
+      'Task doesn\'t exist or user is forbidden to view this task'
+
+        const task = await taskModel.findJoinCreatorApartment({ id: taskId })
+
+        if (
+            !task ||
+      !request.account.apartment ||
+      task.creator.apartment_id != request.account.apartment.id
+        ) {
+            return processClientError(response, 403, errorMessage)
         }
         next()
     } catch (err) {
@@ -65,11 +109,9 @@ async function permissionValidator(
 
 tasksRouter.post(
     '/',
+    taskPropertyValidator,
     middleware.accountExtractor,
     async (req: RequestAfterExtractor, res, next) => {
-        if (!validateTaskProperty(req.body)) {
-            return processClientError(res, 400, 'Task property is invalid')
-        }
         const taskProperty = parseTaskProperty(req.body)
 
         const assignerUsernames: string[] = req.body.assigners
@@ -138,9 +180,10 @@ tasksRouter.post(
 
 tasksRouter.put(
     '/:id',
+    taskPropertyValidator,
     middleware.accountExtractor,
     middleware.paramsIdValidator,
-    permissionValidator,
+    updateDeletePermissionValidator,
     async (req: RequestAfterExtractor, res, next) => {
         const taskId = Number(req.params.id)
 
@@ -156,7 +199,6 @@ tasksRouter.put(
             )
             return res.status(200).json(updatedTask)
         } catch (err) {
-            logger.error(err)
             next(err)
         }
     },
@@ -166,6 +208,7 @@ tasksRouter.delete(
     '/:id',
     middleware.accountExtractor,
     middleware.paramsIdValidator,
+    updateDeletePermissionValidator,
     async (req: RequestAfterExtractor, res, next) => {
         const taskId = Number(req.params.id)
 
@@ -182,12 +225,17 @@ tasksRouter.delete(
 )
 
 tasksRouter.get(
-    '/:id/orders',
+    '/:id',
     middleware.accountExtractor,
     middleware.paramsIdValidator,
-    permissionValidator,
+    viewPermissionValidator,
     async (req: RequestAfterExtractor, res, next) => {
         const taskId = Number(req.params.id)
+        try {
+            const task = await task
+        } catch (err) {
+            next(err)
+        }
     },
 )
 
@@ -195,9 +243,15 @@ tasksRouter.put(
     '/:id/orders',
     middleware.accountExtractor,
     middleware.paramsIdValidator,
-    permissionValidator,
+    viewPermissionValidator,
     async (req: RequestAfterExtractor, res, next) => {
         const taskId = Number(req.params.id)
+        try {
+            const responseTaskRequests =
+        await taskRequestModel.findResponseTaskRequests({ task_id: taskId })
+        } catch (err) {
+            next(err)
+        }
     },
 )
 
