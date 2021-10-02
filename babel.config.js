027864cfccbe299877bf1902c5c744011bbb92ed
anhtumai <tu.mai@outlook.com>
2021-10-02 19:03:33 +0300

    Remove redundant packages

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-    presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript',
-    ],
-    plugins: [],
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
+  plugins: [],
 }
