8b3c5b26a1e088e9e5876d0b4751b3b7c2b3854b
anhtumai <tu.mai@outlook.com>
2021-10-02 18:40:10 +0300

    logger, add winston create logger

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,12 +1,21 @@
-function info(...params: any[]): void {
-  console.log(...params)
-}
+import winston from 'winston'
 
-function error(...params: any[]): void {
-  console.error(...params)
-}
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.simple(),
+    winston.format.timestamp(),
+    winston.format.printf((info) => `[${info.timestamp}][${info.level}] - ${info.message}`)
+  ),
+  transports: [
+    //
+    // - Write all logs with level `error` and below to `error.log`
+    // - Write all logs with level `info` and below to `combined.log`
+    //
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+  ],
+})
 
-export default {
-  info,
-  error,
-}
+export default logger
